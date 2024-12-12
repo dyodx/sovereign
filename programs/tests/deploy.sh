ed0757e46916f9210661ac53f49cd3c34f4f61f7
spacemandev <devbharel@gmail.com>
2024-12-12 14:13:41 -0600

    working test

--- programs/tests/deploy.sh
+++ programs/tests/deploy.sh
@@ -3,7 +3,7 @@
 # Start solana-test-validator in background
 echo "Starting solana-test-validator..."
 solana-test-validator --reset \
-    --bpf-program ./deps/mpl_core.so \
+    --bpf-program "CoREENxT6tW1HoK8ypY1SxRMZTcVPm7R94rH4PZNhX7d" ./deps/mpl_core.so \
     > /dev/null 2>&1 &
 sleep 5
 
