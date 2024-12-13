add5309684b4611e65d854e2a67368056fa06212
jollygrin <JollyGrin@protonmail.com>
2024-12-13 04:18:47 +0100

    setup boxes

--- client/src/routes/dash/+page.svelte
+++ client/src/routes/dash/+page.svelte
@@ -3,12 +3,12 @@
 	import News from './News.svelte';
 </script>
 
-<div class="grid h-screen w-screen grid-cols-[2fr_1fr] grid-rows-[5rem_1fr]">
+<div class="grid h-screen w-screen grid-cols-[2fr_1fr] grid-rows-[5rem_1fr] overflow-x-hidden">
 	<div class="flex items-center border-b-8 border-r-8 border-b-panel border-r-panel px-4">
 		<p class="text-4xl font-bold text-panel">SOVEREIGN</p>
 	</div>
 	<div class="flex items-center justify-center border-b-8 border-b-panel">
-		<p class="text-4xl font-bold text-panel">NEWS</p>
+		<p class=" text-4xl font-bold text-panel">NEWS</p>
 	</div>
 
 	<div class="border-r-8 border-r-panel">
--- client/src/routes/dash/Body.svelte
+++ client/src/routes/dash/Body.svelte
@@ -1,3 +1,7 @@
+<script>
+	import Citizens from './Citizens/Citizens.svelte';
+</script>
+
 <div class="flex h-full flex-col gap-8">
 	<div class="flex flex-col gap-4 p-4">
 		<div class="shadow-flat flex flex-col gap-2 rounded-xl border-2 border-black bg-panel p-4">
@@ -6,7 +10,7 @@
 				<div class="flex gap-[-10px]">
 					{#each Array.from({ length: 8 }).map((_, i) => i) as slotNumber}
 						<div
-							class="ml-[-1rem] h-10 w-10 overflow-clip rounded-full border-2 border-panel bg-foreground"
+							class="ml-[-1rem] h-10 w-10 overflow-hidden rounded-full border-2 border-panel bg-foreground"
 						>
 							<img
 								src={`https://api.dicebear.com/9.x/lorelei-neutral/svg?seed=${slotNumber}`}
@@ -30,5 +34,7 @@
 		</div>
 	</div>
 
-	<div class="flex-grow border-t-8 border-t-panel bg-black">j</div>
+	<div class="h-[400px] flex-grow overflow-y-auto border-t-8 border-t-panel bg-black p-8">
+		<Citizens />
+	</div>
 </div>
