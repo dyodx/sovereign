1afc3d2946588635b6728cd8d31246461c8810fb
jollygrin <JollyGrin@protonmail.com>
2024-12-13 17:50:09 +0100

    update to prepare modal

---
+++ client/src/routes/dash/Modals/ModalRecruit.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	let { showModal = $bindable() } = $props();
+
+	let dialog: HTMLDialogElement | undefined = $state(); // HTMLDialogElement
+
+	$effect(() => {
+		if (showModal) dialog?.showModal();
+	});
+</script>
+
+<dialog
+	bind:this={dialog}
+	onclose={() => (showModal = false)}
+	onclick={(e) => {
+		if (e.target === dialog) dialog.close();
+	}}
+	class="bg-white p-4"
+>
+	<div></div>
+</dialog>
--- client/src/routes/dash/Panels/MintNewCitizen.svelte
+++ client/src/routes/dash/Panels/MintNewCitizen.svelte
@@ -1,3 +1,19 @@
+<script lang="ts">
+	import ModalRecruit from '../Modals/ModalRecruit.svelte';
+
+	let showModal = $state(false);
+	let numberOfCitizens = $state(1);
+	function increment() {
+		numberOfCitizens++;
+	}
+
+	function decrement() {
+		if (numberOfCitizens > 1) {
+			numberOfCitizens--;
+		}
+	}
+</script>
+
 <p class="text-light text-4xl">Recruit</p>
 <p class="font-light">
 	Recruit a citizen to join your efforts. Purchase a job listing to get a citizen on retainer.
@@ -6,14 +22,25 @@
 <p class="mb-2 mt-8 text-xl">Purchase a job listing to recruit a citizen:</p>
 
 <div class="flex w-full flex-col gap-2 sm:flex-row sm:items-center">
-	<div class="flex gap-2">
-		<button class="rounded-lg bg-background px-4 py-2 transition-all hover:bg-black"> + </button>
-		<button class="rounded-lg bg-background px-4 py-2 transition-all hover:bg-black"> - </button>
+	<div class="flex gap-2 sm:mr-2 sm:border-r-2 sm:border-r-background sm:pr-4">
+		<button
+			onclick={increment}
+			class="rounded-lg bg-background px-4 py-2 transition-all hover:bg-black"
+		>
+			+
+		</button>
+		<button
+			onclick={decrement}
+			class="rounded-lg bg-background px-4 py-2 transition-all hover:bg-black"
+		>
+			-
+		</button>
 	</div>
 	<p>Recruit</p>
 	<input
+		bind:value={numberOfCitizens}
 		type="number"
-		value="1"
+		min={1}
 		class="max-w-[4rem] rounded-xl bg-background p-1 text-center text-lg"
 	/>
 	<p>citizen for</p>
@@ -32,3 +59,4 @@
 >
 	Prepare purchase
 </button>
+<ModalRecruit bind:showModal />
