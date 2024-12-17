9b328ae41537fb3670f65fb6b141ce661ef7a81a
jollygrin <JollyGrin@protonmail.com>
2024-12-17 16:01:32 +0100

    add table to balances

--- client/src/routes/dash/Body.svelte
+++ client/src/routes/dash/Body.svelte
@@ -1,12 +1,16 @@
 <script>
 	import Citizens from './Citizens/Citizens.svelte';
-	import MintNewCitizen from './Panels/MintNewCitizen.svelte';
+	import Panel_Balances from './Panels/Balances.svelte';
+	import Panel_MintNewCitizen from './Panels/MintNewCitizen.svelte';
 </script>
 
 <div class="flex h-full flex-col gap-8">
 	<div class="flex flex-col gap-4 p-4">
-		<div class=" gap-2 rounded-xl border-2 border-black bg-panel p-4 shadow-flat sm:max-w-[70%]">
-			<MintNewCitizen />
+		<div class="rounded-xl border-2 border-black bg-panel p-4 shadow-flat sm:max-w-[70%]">
+			<Panel_MintNewCitizen />
+		</div>
+		<div class="rounded-xl border-2 border-black bg-panel p-4 shadow-flat sm:max-w-[70%]">
+			<Panel_Balances />
 		</div>
 	</div>
 
---
+++ client/src/routes/dash/Panels/Balances.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+	import * as Table from '$lib/components/ui/table';
+	import TipCitizensExplained from '../Tooltips/TipCitizensExplained.svelte';
+
+	let data = [
+		{
+			ticker: 'USA',
+			balance: 1000,
+			perSol: 0.0025
+		}
+	];
+</script>
+
+<div class="flex justify-between">
+	<p class="text-light text-2xl md:text-4xl">Balances</p>
+	<TipCitizensExplained />
+</div>
+
+<Table.Root>
+	<Table.Caption class="py-2">view all coins</Table.Caption>
+	<Table.Header>
+		<Table.Row>
+			<Table.Head>COIN</Table.Head>
+			<Table.Head class="text-right">Per SOL</Table.Head>
+			<Table.Head class="text-right">Amount</Table.Head>
+		</Table.Row>
+	</Table.Header>
+	<Table.Body>
+		{#each data as { ticker, balance, perSol }}
+			<Table.Row>
+				<Table.Cell>
+					{ticker}
+				</Table.Cell>
+				<Table.Cell class="text-right">{perSol}</Table.Cell>
+				<Table.Cell class="text-right">{balance}</Table.Cell>
+			</Table.Row>
+		{/each}
+	</Table.Body>
+</Table.Root>
