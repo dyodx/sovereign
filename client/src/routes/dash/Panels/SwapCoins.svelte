778314a2abee22c1f307f715a0520c19adf1ada2
jollygrin <JollyGrin@protonmail.com>
2024-12-17 21:44:39 +0100

    prepare swap

--- client/src/routes/dash/Panels/SwapCoins.svelte
+++ client/src/routes/dash/Panels/SwapCoins.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
-	import * as Table from '$lib/components/ui/table';
 	import TipCitizensExplained from '../Tooltips/TipCitizensExplained.svelte';
+	import Combobox from '$lib/components/molecules/Combobox/Combobox.svelte';
+	import * as Popover from '$lib/components/ui/popover';
 </script>
 
 <div class="flex justify-between">
@@ -9,9 +10,10 @@
 </div>
 
 <div class="flex flex-col items-center py-4">
+	<Combobox></Combobox>
 	<div class="flex">
 		<input class="rounded-xl bg-background p-1 text-center text-lg" />
-		<input class="max-w-20 rounded-xl bg-background p-1 text-center text-lg" />
+		<!-- <input class="max-w-20 rounded-xl bg-background p-1 text-center text-lg" /> -->
 	</div>
 	<div class="z-50 my-[-0.5rem] h-8 w-8 rounded-lg border-4 border-panel bg-background"></div>
 	<input class="rounded-xl bg-background p-1 text-center text-lg" />
