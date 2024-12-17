02321d58fedb4f5b80aa0396d548bbc72baee620
jollygrin <JollyGrin@protonmail.com>
2024-12-17 14:13:33 +0100

    update mobile with tabs and clean up ordering

--- client/src/routes/dash/+page.svelte
+++ client/src/routes/dash/+page.svelte
@@ -9,24 +9,24 @@
 <div
 	class="grid h-screen w-screen md:grid-cols-[2fr_1fr] md:grid-rows-[5rem_1fr] md:overflow-x-hidden"
 >
-	<div class="items-center border-b-8 border-b-panel border-r-panel px-4 md:flex md:border-r-8">
+	<div
+		class="order-last items-center border-t-8 border-panel border-b-panel px-4 md:order-first md:flex md:border-b-8 md:border-r-8 md:border-t-0"
+	>
 		<p class="hidden text-4xl font-bold text-panel md:inline">SOVEREIGN</p>
 		<Tabs.Root bind:value={tab} class="w-full py-4 md:hidden">
 			<Tabs.List class="justify-self-end">
 				<Tabs.Trigger value="body">Dashboard</Tabs.Trigger>
 				<Tabs.Trigger value="news">News</Tabs.Trigger>
 			</Tabs.List>
-			<!-- <Tabs.Content value="account">Make changes to your account here.</Tabs.Content> -->
-			<!-- <Tabs.Content value="password">Change your password here.</Tabs.Content> -->
 		</Tabs.Root>
 	</div>
-	<div class="hidden items-center justify-center border-b-8 border-b-panel sm:flex">
-		<p class=" text-4xl font-bold text-panel">NEWS</p>
+	<div class="hidden items-center justify-center border-b-8 border-b-panel md:flex">
+		<p class="text-4xl font-bold text-panel">NEWS</p>
 	</div>
 
 	<!-- BOTTOM SECTION -->
 	<div
-		class={`overflow-y-auto border-r-panel sm:h-[calc(100vh-5rem)] md:overflow-y-visible md:border-r-8`}
+		class={`overflow-y-auto overflow-x-hidden border-r-panel md:h-[calc(100vh-5rem)] md:overflow-x-visible md:overflow-y-visible md:border-r-8`}
 	>
 		{#if tab === 'body'}
 			<Body />
@@ -36,7 +36,7 @@
 			</div>
 		{/if}
 	</div>
-	<div class="hidden w-full flex-col gap-4 overflow-y-auto overflow-x-hidden p-4 sm:flex">
+	<div class="hidden w-full flex-col gap-4 overflow-y-auto overflow-x-hidden p-4 md:flex">
 		<News />
 	</div>
 </div>
