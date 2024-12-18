782f31cb229e42c2591c54b1c4328dcca95306c5
jollygrin <JollyGrin@protonmail.com>
2024-12-18 17:13:39 +0100

    buttons

--- client/src/routes/dash/+page.svelte
+++ client/src/routes/dash/+page.svelte
@@ -3,6 +3,19 @@
 	import Body from './Body.svelte';
 	import News from './News.svelte';
 	import * as Tabs from '$lib/components/ui/tabs';
+	import { getAppKit } from '$lib/wallet/appkit';
+	import type { AppKit } from '@reown/appkit';
+
+	let appkit = getAppKit() as AppKit;
+
+	function login() {
+		appkit?.open();
+	}
+	function openAccount() {
+		appkit?.open({ view: 'Account' });
+	}
+
+	let address = appkit?.getAddress() ?? '';
 
 	let tab: 'dash' | 'news' | 'state' = $state('dash');
 </script>
@@ -11,31 +24,59 @@
 	class="grid h-screen w-screen md:grid-cols-[2fr_1fr] md:grid-rows-[5rem_1fr] md:overflow-x-hidden"
 >
 	<div
-		class="order-last items-center border-t-8 border-panel border-b-panel px-4 md:order-first md:flex md:border-b-8 md:border-r-8 md:border-t-0"
+		class="order-last justify-between border-t-8 border-panel border-b-panel px-4 md:order-first md:flex md:border-b-8 md:border-r-8 md:border-t-0"
 	>
-		<button
-			class={cn(
-				'hidden text-4xl font-bold md:inline',
-				tab === 'dash' ? 'text-foreground' : 'text-panel'
-			)}
-			onclick={() => {
-				tab = 'dash';
-			}}
-		>
-			SOVEREIGN
-		</button>
-		<span class="hidden text-4xl font-bold text-panel md:ml-4 md:inline"> | </span>
-		<button
-			class={cn(
-				'hidden text-4xl font-bold md:inline',
-				tab === 'state' ? 'text-foreground' : 'text-panel'
-			)}
-			onclick={() => {
-				tab = 'state';
-			}}
-		>
-			NATION STATE
-		</button>
+		<div class="flex items-center">
+			<button
+				class={cn(
+					'hidden text-4xl font-bold md:inline',
+					tab === 'dash' ? 'text-foreground' : 'text-panel'
+				)}
+				onclick={() => {
+					tab = 'dash';
+				}}
+			>
+				SOVEREIGN
+			</button>
+			<span class="hidden text-4xl font-bold text-panel md:ml-2 md:inline"> | </span>
+			<button
+				class={cn(
+					'hidden text-4xl font-bold md:inline',
+					tab === 'state' ? 'text-foreground' : 'text-panel'
+				)}
+				onclick={() => {
+					tab = 'state';
+				}}
+			>
+				NATION STATE
+			</button>
+		</div>
+		<div class="flex items-center">
+			{#if !!address}
+				<button onclick={openAccount} class="rounded-xl bg-panel px-4 py-2">
+					<div class="group flex items-center gap-4">
+						<span class="tracking-tight">
+							{address.substring(0, 4)}
+							{address.substring(address.length - 4, address.length)}
+						</span>
+						<img
+							class="scale-100 rounded-full bg-background bg-center p-[2px] transition-all group-hover:scale-125"
+							src={`https://api.dicebear.com/9.x/identicon/svg?seed=${address}`}
+							alt="pfp"
+							width="35px"
+							height="35px"
+						/>
+					</div>
+				</button>
+			{:else}
+				<button
+					onclick={login}
+					class="scale-100 rounded-xl bg-panel px-4 py-2 group-hover:scale-110"
+				>
+					login
+				</button>
+			{/if}
+		</div>
 		<Tabs.Root bind:value={tab} class="w-full py-4 md:hidden">
 			<Tabs.List class="justify-self-end">
 				<Tabs.Trigger value="dash">Dashboard</Tabs.Trigger>
