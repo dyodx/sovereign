2906513e92b2b1057a95eb03d3b3d9720a9f469a
spacemandev <devbharel@gmail.com>
2024-12-19 01:39:00 -0600

    edgedb

--- programs/tests/deploy.sh
+++ programs/tests/deploy.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-# Start solana-test-validator in background
+anchor build
+
 echo "Starting solana-test-validator..."
 solana-test-validator --reset \
     --bpf-program "CoREENxT6tW1HoK8ypY1SxRMZTcVPm7R94rH4PZNhX7d" ./deps/mpl_core_program.so \
+    --bpf-program "4oVhv3o16X3XR99UgbFrWNKptoNBkg2hsbNY2nYPpv4a" ./target/deploy/programs.so \
     > /dev/null 2>&1 &
 sleep 5
 
@@ -11,13 +13,9 @@ sleep 5
 solana config set --url localhost
 
 echo "Airdropping SOL to admin keypair..."
-solana airdrop 100 -k "./localnet-keys/admin.json"
+solana airdrop 10000 -k "./localnet-keys/admin.json"
 sleep 5
 
-# Deploy program 
-echo "Deploying program..."
-solana program deploy ./target/deploy/programs.so --program-id ./localnet-keys/deploy-key.json
-sleep 2
 
 echo "Deployment successful!"
 
