4db4114975222b68288b3089761ae3af0f8a12dc
jollygrin <JollyGrin@protonmail.com>
2024-12-20 18:52:04 +0100

    legggggo

--- client/src/routes/privy/+page.svelte
+++ client/src/routes/privy/+page.svelte
@@ -1,7 +1,12 @@
 <script lang="ts">
+	import { page } from '$app/stores';
 	import Privy, { LocalStorage } from '@privy-io/js-sdk-core';
 	import { redirect } from '@sveltejs/kit';
 	import { onMount } from 'svelte';
+
+	let privy_oauth_state = $page.url.searchParams.get('privy_oauth_state');
+	let privy_oauth_provider = $page.url.searchParams.get('privy_oauth_provider');
+	let privy_oauth_code = $page.url.searchParams.get('privy_oauth_code');
 
 	let privy: Privy | null = $state(null);
 	let twitterAuth: '';
@@ -13,7 +18,7 @@
 		});
 		privy = newPrivy;
 
-		const twitttt = await privy?.auth.oauth.generateURL('twitter', 'http://localhost:5173/privy');
+		const twitttt = await privy?.auth.oauth.generateURL('twitter', 'http://localhost:5173/dash');
 		console.log({ twitttt });
 		twitterAuth = twitttt.url;
 
@@ -22,14 +27,27 @@
 </script>
 
 <div>
-	<!-- {privy?.embeddedWallet?.getURL()} -->
-
-	<!-- {privy?.auth.oauth.generateURL('twitter', 'https://localhost:5173/auth/twitter/callback')} -->
+	{privy_oauth_code}
+	{privy_oauth_state}
+	{privy_oauth_provider}
 	{#await privy?.auth.oauth.generateURL('twitter', 'http://localhost:5173/privy') then url}
-		{url?.url}
-
 		<a href={url?.url}>
 			<button> login </button>
 		</a>
 	{/await}
+
+	<button
+		onclick={async () => {
+			const res = (privy as Privy).auth.oauth.loginWithCode(
+				privy_oauth_code as string,
+				privy_oauth_state as string,
+				'twitter'
+			);
+			res.then((e) => {
+				console.log(e);
+			});
+		}}
+	>
+		auth
+	</button>
 </div>
