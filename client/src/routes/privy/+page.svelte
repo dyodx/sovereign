14ebfc68c7c04d9695dfd363b88ca9279593ddc7
spacemandev <devbharel@gmail.com>
2024-12-20 14:30:12 -0600

    working code

--- client/src/routes/privy/+page.svelte
+++ client/src/routes/privy/+page.svelte
@@ -2,12 +2,12 @@
 	import { page } from '$app/stores';
 	import Privy, { getAllUserEmbeddedSolanaWallets, getUserEmbeddedEthereumWallet, getUserEmbeddedSolanaWallet, LocalStorage, type PrivyEmbeddedSolanaWalletProvider } from '@privy-io/js-sdk-core';
 	import type { PrivyAuthenticatedUser } from '@privy-io/public-api';
-	import { Connection, PublicKey, SystemProgram, Transaction } from '@solana/web3.js';
+	import { ComputeBudgetProgram, Connection, PublicKey, SystemProgram, Transaction, TransactionMessage, VersionedTransaction } from '@solana/web3.js';
 	import { onDestroy, onMount } from 'svelte';
-
+	
 	let privy_oauth_state = $page.url.searchParams.get('privy_oauth_state');
 	let privy_oauth_code = $page.url.searchParams.get('privy_oauth_code');
-
+	
 	let privy: Privy | null = $state(null);
 	let twitURL = $state("")
 	let user = $state(null as PrivyAuthenticatedUser | null)
@@ -30,28 +30,28 @@
 			window.addEventListener('message', handler);
 		}
 	});
-
+	
 	onDestroy(() => {
 		if(typeof window !== 'undefined' && handler) {
 			window.removeEventListener('message', handler)
 		}
 	});
-
+	
 	async function generateURL() {
 		twitURL = (await privy?.auth.oauth.generateURL('twitter', 'http://localhost:5173/privy'))!.url as string;
 	}
-
+	
 	async function loginWithCode() {
 		privy?.auth.oauth.loginWithCode(
-			privy_oauth_code as string,
-			privy_oauth_state as string,
-			'twitter'
+		privy_oauth_code as string,
+		privy_oauth_state as string,
+		'twitter'
 		).then((e) => {
 			user = e;
 			console.log(user);
 		});
 	} 
-
+	
 	async function createEmbeddedWallet() {
 		if(privy?.embeddedWallet.hasEmbeddedWallet()) {
 			const accounts = getAllUserEmbeddedSolanaWallets(user!.user);
@@ -59,32 +59,63 @@
 			const provider = await privy.embeddedWallet.getSolanaProvider(accounts[0], accounts[0].address, "solana-address-verifier");
 			const connection = new Connection("https://api.devnet.solana.com");
 			const pkey = new PublicKey(address);
+			
+			const tx = new VersionedTransaction(new TransactionMessage({
+				payerKey: pkey,
+				recentBlockhash: (await connection.getLatestBlockhash()).blockhash,
+				instructions: [
+					ComputeBudgetProgram.setComputeUnitPrice({microLamports: 1_000_000}),
+					ComputeBudgetProgram.setComputeUnitLimit({units: 100000}),
+					SystemProgram.transfer({
+						fromPubkey: pkey,
+						toPubkey: pkey,
+						lamports: 1
+					})
+				]
+			}).compileToV0Message());    
+			const serialized = Buffer.from(tx.serialize()).toString('base64');
+			console.log("Pre signed: ", serialized);
+			const signature = (await provider.request({
+				method: "signMessage",
+				params: {
+					message: serialized
+				}
+			})).signature;
+			tx.addSignature(pkey, Uint8Array.from(Buffer.from(signature, "base64")));
+			console.log("Signed: ", Buffer.from(tx.serialize()).toString('base64'));
+			const txnSig = await connection.sendRawTransaction(tx.serialize());
+			console.log("txnSig: ", txnSig);
+
+
+			/*	
 			const transaction = new Transaction()
 			transaction.feePayer = pkey;
 			transaction.recentBlockhash = (await connection.getLatestBlockhash()).blockhash;
 			transaction.add(
-				SystemProgram.transfer({
-					fromPubkey: pkey,
-					toPubkey: pkey,
-					lamports: 1
-				})
+			SystemProgram.transfer({
+			fromPubkey: pkey,
+			toPubkey: pkey,
+			lamports: 1
+			})
 			)
 			const serializedTransaction = transaction.serialize({requireAllSignatures: false, verifySignatures: false}).toString('base64');
+			console.log(serializedTransaction);
 			console.log("trying things");
 			const signedTxn = (await provider.request({
-				method: "signMessage",
-				params: {
-					message: serializedTransaction
-				}
+			method: "signMessage",
+			params: {
+			message: serializedTransaction
+			}
 			})).signature;
 			console.log(signedTxn);
 			const txnSig = await connection.sendRawTransaction(Uint8Array.from(atob(signedTxn), c => c.charCodeAt(0)));
 			console.log(txnSig);
-
+			*/
+			
 		} else {
 			embeddedWallet = (await privy?.embeddedWallet.createSolana())!.provider;
 		}
-
+		
 	}
 </script>
 
@@ -94,10 +125,10 @@
 	<button onclick={loginWithCode}> loginWithCode {privy_oauth_state} {privy_oauth_code} </button> <br />
 	<button onclick={createEmbeddedWallet}> createEmbeddedWallet {user?.user.id} </button> <br />
 	<iframe
-	  bind:this={iframe}
-	  src={iframeSrc}
-	  style="width: 0; height: 0; border: none;"
-	  title="Privy embedded wallet"
+	bind:this={iframe}
+	src={iframeSrc}
+	style="width: 0; height: 0; border: none;"
+	title="Privy embedded wallet"
 	>
-	</iframe>
+</iframe>
 </div>
