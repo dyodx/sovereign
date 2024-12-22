c59dccaaf54283ccfb4b6d419691af19bc3df7a3
jollygrin <JollyGrin@protonmail.com>
2024-12-22 17:41:11 +0100

    update recruit modal

--- client/src/routes/dash/Modals/ModalRecruit.svelte
+++ client/src/routes/dash/Modals/ModalRecruit.svelte
@@ -5,11 +5,13 @@
 	import { PUBLIC_RPC_URL } from '$env/static/public';
 	import { walletStore } from '$lib/stores/wallet.svelte';
 	import { privyStore } from '$lib/stores/privy.svelte';
-	import { Connection, PublicKey } from '@solana/web3.js';
+	import { Connection, LAMPORTS_PER_SOL, PublicKey } from '@solana/web3.js';
 	import Privy from '@privy-io/js-sdk-core';
 	import { walletHandler } from '$lib/wallet/walletHelpers';
 	import type { PrivyAuthenticatedUser } from '@privy-io/public-api';
-	import { getPlayerAccount } from '$lib/wallet/txUtilities';
+	import { getGameAccount, getPlayerAccount } from '$lib/wallet/txUtilities';
+	import IconSolana from '$lib/components/atoms/icons/IconSolana.svelte';
+	import { copyToClipboard } from '$lib/utils';
 
 	let { numberOfCitizens = 1, children } = $props();
 
@@ -35,32 +37,19 @@
 
 	let confirmedTx = $state('');
 
+	async function getMintCost() {
+		const { getGameMetaData } = getGameAccount();
+		const gameMetaData = await getGameMetaData();
+		const mintCost = gameMetaData.mintCost / LAMPORTS_PER_SOL;
+		return mintCost;
+	}
+
 	async function createEmbeddedWallet() {
 		await walletHandler.createEmbeddedWallet({
 			privy: privy as Privy,
 			user: user as PrivyAuthenticatedUser,
 			setProvider: (e) => (provider = e)
 		});
-	}
-
-	async function sendOneLamportToSelf() {
-		if (!provider) {
-			console.error('no provider');
-			await createEmbeddedWallet();
-			sendOneLamportToSelf();
-			return;
-		}
-		if (!address || address === '')
-			return console.error('Sending Lamport Error: no address:', address);
-		const connection = new Connection(PUBLIC_RPC_URL as string);
-		const { tx, message } = await buildTransaction.sendOneLamportToSelf(connection, address);
-		const signed = await buildRequest(provider, message, address);
-
-		const pkey = new PublicKey(address);
-		tx.addSignature(pkey, Uint8Array.from(Buffer.from(signed, 'base64')));
-
-		const confirmedSentTx = await connection.sendTransaction(tx);
-		confirmedTx = confirmedSentTx;
 	}
 
 	async function mintNewCitizen() {
@@ -83,6 +72,35 @@
 	}
 </script>
 
+{#snippet mint()}
+	<div class="relative h-[200px] rounded-xl bg-panel">
+		<div class="absolute bottom-0 flex w-full items-center justify-center py-2">
+			{#await getMintCost() then mintCost}
+				<IconSolana />
+				<p class="text-xl font-bold">{mintCost}</p>
+			{/await}
+		</div>
+	</div>
+	<button
+		onclick={mintNewCitizen}
+		class="mt-4 w-full rounded-xl border-2 border-black bg-black p-2 transition-all hover:bg-black disabled:opacity-75"
+	>
+		{#if confirmedTx === ''}
+			Purchase
+		{:else}
+			Purchased another
+		{/if}
+	</button>
+
+	{#if confirmedTx !== ''}
+		<button
+			onclick={() => copyToClipboard(confirmedTx)}
+			class="max-w-[300px] overflow-x-auto pt-2 underline"
+			>Copy Tx Hash: {confirmedTx.substring(0, 6)}...</button
+		>
+	{/if}
+{/snippet}
+
 <Dialog.Root>
 	<Dialog.Trigger class="w-full">
 		{@render children?.()}
@@ -99,18 +117,7 @@
 							{#if !data?.Account?.xUsername}
 								<p>You must register your twitter account to recruit citizens</p>
 							{:else}
-								<p>Address: {$walletStore.address}</p>
-								<p class="max-w-[300px] overflow-x-auto">Confirm: {confirmedTx}</p>
-								<button
-									onclick={mintNewCitizen}
-									class="mt-4 w-full rounded-xl border-2 border-black bg-black p-2 transition-all hover:bg-black disabled:opacity-75"
-								>
-									{#if confirmedTx === ''}
-										Purchase
-									{:else}
-										Purchased another
-									{/if}
-								</button>
+								{@render mint()}
 							{/if}
 						{/await}
 					{/if}
