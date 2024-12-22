e812ef4e80e392173be1483aaae6123f86127f90
jollygrin <JollyGrin@protonmail.com>
2024-12-22 15:23:11 +0100

    clean up register twitter

--- client/src/routes/dash/+page.svelte
+++ client/src/routes/dash/+page.svelte
@@ -11,7 +11,7 @@
 	import States from './States.svelte';
 	import IconTwitter from '$lib/components/atoms/icons/IconTwitter.svelte';
 	import { getPlayerAccount } from '$lib/wallet/txUtilities';
-	import RegisterTwitter from '$lib/components/molecules/RegisterTwitter/RegisterTwitter.svelte';
+	import ModalRegisterTwitter from '$lib/components/molecules/RegisterTwitter/ModalRegisterTwitter.svelte';
 
 	let privy: Privy | null = $state(null);
 	$inspect('privy', privy);
@@ -104,23 +104,27 @@
 		</div>
 		<div class="flex items-center gap-2 pt-4 md:pt-0">
 			{#if !!address}
+				<!-- TWITTER REGISTER: show if twitter is not linked -->
 				{#await getPlayerAccount(address) then data}
-					{console.log({ data })}
-					<div>
-						<RegisterTwitter>
-							<button
-								class="relative rounded-xl bg-yellow-500 p-3 text-black transition-all hover:scale-110 active:scale-90"
-							>
-								<span
-									class="absolute left-[-8px] top-[-8px] rounded-lg bg-green-300 px-[8px] py-[1px]"
+					{#if !data?.Account?.xUsername}
+						<div>
+							<ModalRegisterTwitter>
+								<button
+									class="relative rounded-xl bg-yellow-500 p-3 text-black transition-all hover:scale-110 active:scale-90"
 								>
-									!
-								</span>
-								<IconTwitter />
-							</button>
-						</RegisterTwitter>
-					</div>
+									<span
+										class="absolute left-[-8px] top-[-8px] rounded-lg bg-green-300 px-[8px] py-[1px]"
+									>
+										!
+									</span>
+									<IconTwitter />
+								</button>
+							</ModalRegisterTwitter>
+						</div>
+					{/if}
 				{/await}
+				<!-- END || TWITTER REGISTER: show if twitter is not linked -->
+
 				<button onclick={openAccount} class="rounded-xl bg-panel px-4 py-2">
 					<div class="group flex items-center gap-4">
 						<span class="tracking-tight">
