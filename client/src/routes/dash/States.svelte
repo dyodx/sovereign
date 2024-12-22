c24421052b263c4024154b0ba8378f970cd78228
jollygrin <JollyGrin@protonmail.com>
2024-12-22 22:10:28 +0100

    setup fetch

--- client/src/routes/dash/States.svelte
+++ client/src/routes/dash/States.svelte
@@ -1,10 +1,39 @@
 <script>
+	import * as Table from '$lib/components/ui/table';
 	import TipOperations from './Tooltips/TipOperations.svelte';
+	import { api } from '$lib/services/apiClient';
+
+	import { createQuery } from '@tanstack/svelte-query';
+	const query = createQuery({
+		queryKey: ['states'],
+		queryFn: api.fetch.getNations
+	});
+
+	$inspect('data', $query?.data);
 </script>
 
 <div class="grid min-h-[500px] place-items-center rounded-xl bg-panel text-foreground">
-	nation table stats
+	<Table.Root>
+		<Table.Caption class="py-2">view all nations</Table.Caption>
+		<Table.Header>
+			<Table.Row>
+				<Table.Head>COIN</Table.Head>
+				<Table.Head class="text-right">COIN/SOL</Table.Head>
+				<Table.Head class="text-right">Amount</Table.Head>
+				<Table.Head class="text-right">SOL</Table.Head>
+			</Table.Row>
+		</Table.Header>
+		<Table.Body>
+			<Table.Row>
+				<Table.Cell>x</Table.Cell>
+				<Table.Cell class="text-right">{'x'}</Table.Cell>
+				<Table.Cell class="text-right">{'x'}</Table.Cell>
+				<Table.Cell class="text-right">{'x'}</Table.Cell>
+			</Table.Row>
+		</Table.Body>
+	</Table.Root>
 </div>
+
 <hr class="my-4 rounded border-2 border-panel" />
 <div class="flex flex-wrap justify-between gap-2">
 	<p class="mb-0 text-xl">Incoming Operation Requests</p>
