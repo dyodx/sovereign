c8e2162ec21f43a7c92e9701ff1e9f7b6ea184f0
jollygrin <JollyGrin@protonmail.com>
2024-12-22 20:35:55 +0100

    account for 0 javascript

--- backend/src/api/index.ts
+++ backend/src/api/index.ts
@@ -4,62 +4,69 @@ import { CONNECTION, DB, SERVER_URL } from "../common.ts";
 
 const app = new Hono();
 export default {
-    port: 5600,
-    fetch: app.fetch,
+  port: 5600,
+  fetch: app.fetch,
 };
 
 /// ROUTES ///
 app.get("/", (c) => c.text("Hello World")); // heartbeat
 
 app.get("/airdrop", async (c) => {
-    const publicKey = c.req.param("publicKey");
-    if (!publicKey) {
-        return c.text("Missing publicKey parameter", 400);
-    }
+  const publicKey = c.req.param("publicKey");
+  if (!publicKey) {
+    return c.text("Missing publicKey parameter", 400);
+  }
 
-    try {
-        const signature = await CONNECTION.requestAirdrop(new PublicKey(publicKey), 10*LAMPORTS_PER_SOL); //10 SOL
-        return c.json({ signature });
-    } catch (error: any) {
-        return c.text("Failed to request airdrop: " + error.message, 500);
-    }
+  try {
+    const signature = await CONNECTION.requestAirdrop(
+      new PublicKey(publicKey),
+      10 * LAMPORTS_PER_SOL,
+    ); //10 SOL
+    return c.json({ signature });
+  } catch (error: any) {
+    return c.text("Failed to request airdrop: " + error.message, 500);
+  }
 });
 
 // Collection JSON
 app.get("/collection", async (c) => {
-    const gameId = c.req.param("gameId");
-    if (!gameId) {
-        return c.text("Missing gameId parameter", 400);
-    }
+  const gameId = c.req.param("gameId");
+  if (!gameId) {
+    return c.text("Missing gameId parameter", 400);
+  }
 
-    return c.json({
-        name: `Sovereign ${gameId}`,
-        description: `Sovereign ${gameId}`,
-        image: "https://utfs.io/f/HOLEiqUAKjUF7czVJ3wyWNui0pBOk8JVtAjGI24hfKoYEUTF",
-        external_url: `${SERVER_URL}/game/${gameId}`,
-        properties: {
-            files: [
-                {
-                    uri: "https://utfs.io/f/HOLEiqUAKjUF7czVJ3wyWNui0pBOk8JVtAjGI24hfKoYEUTF",
-                    type: "image/png"
-                }
-            ],
-            category: "image",
-        }
-    }, 200);
+  return c.json(
+    {
+      name: `Sovereign ${gameId}`,
+      description: `Sovereign ${gameId}`,
+      image:
+        "https://utfs.io/f/HOLEiqUAKjUF7czVJ3wyWNui0pBOk8JVtAjGI24hfKoYEUTF",
+      external_url: `${SERVER_URL}/game/${gameId}`,
+      properties: {
+        files: [
+          {
+            uri: "https://utfs.io/f/HOLEiqUAKjUF7czVJ3wyWNui0pBOk8JVtAjGI24hfKoYEUTF",
+            type: "image/png",
+          },
+        ],
+        category: "image",
+      },
+    },
+    200,
+  );
 });
 
 app.get("/nations", async (c) => {
-    const gameId = c.req.param("gameId");
-    if (!gameId) {
-        return c.text("Missing gameId parameter", 400);
-    }
+  const gameId = c.req.param("gameId");
+  if (gameId === undefined || gameId === "") {
+    return c.text("Missing gameId parameter", 400);
+  }
 
-    const nations = await DB.nationAccount.findMany({
-        where: {
-            gameId: BigInt(gameId)  
-        }
-    });
+  const nations = await DB.nationAccount.findMany({
+    where: {
+      gameId: BigInt(gameId),
+    },
+  });
 
-    return c.json(nations);
+  return c.json(nations);
 });
