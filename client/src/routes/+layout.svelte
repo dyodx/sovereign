3749fbbbdbef296c506a61d00926943046c02f4d
jollygrin <JollyGrin@protonmail.com>
2024-12-23 01:44:57 +0100

    add store

--- client/src/routes/+layout.svelte
+++ client/src/routes/+layout.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
 	import '../app.css';
 	import { QueryClient, QueryClientProvider } from '@tanstack/svelte-query';
+	import { updateQueryStore } from '$lib/stores/query.svelte';
 
 	//@ts-expect-error: compiler fails to see this dependency
 	import { pwaInfo } from 'virtual:pwa-info';
 	const webManifestLink = $derived(pwaInfo ? pwaInfo.webManifest.linkTag : '');
 
 	const queryClient = new QueryClient();
+	updateQueryStore(queryClient);
 
 	let { children } = $props();
 </script>
