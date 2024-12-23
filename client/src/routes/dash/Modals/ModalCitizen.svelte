8d38b2fcb8f789adf2b4c1a24b41d99f7d77d0d9
jollygrin <JollyGrin@protonmail.com>
2024-12-23 02:20:05 +0100

    add rates

--- client/src/routes/dash/Modals/ModalCitizen.svelte
+++ client/src/routes/dash/Modals/ModalCitizen.svelte
@@ -1,43 +1,31 @@
 <script lang="ts">
 	import * as Dialog from '$lib/components/ui/dialog';
 	import Combobox from '$lib/components/molecules/Combobox/Combobox.svelte';
-	import { currencies } from '$lib/constants/currencies';
 	import { getCountryFlag } from '$lib/constants/flags';
 	import { fetchAssetV1, mplCore, type AssetV1 } from '@metaplex-foundation/mpl-core';
 	import { createUmi } from '@metaplex-foundation/umi-bundle-defaults';
 	import { PUBLIC_RPC_URL } from '$env/static/public';
 	import { generateNamePair } from '$lib/constants/names';
 	import IconCopy from '$lib/components/atoms/icons/IconCopy.svelte';
-	import toast from 'svelte-french-toast';
 	import { CITIZEN_IMG_URL } from '$lib/constants/citizens';
-	import {
-		IconExchange,
-		IconGavel,
-		IconLeaf,
-		IconMoneyBag,
-		IconSolana,
-		IconStethoscope
-	} from '$lib/components/atoms/icons';
+	import { IconGavel, IconLeaf, IconMoneyBag, IconStethoscope } from '$lib/components/atoms/icons';
+	import { NATION_STATES as _NATION_STATES } from '$lib/constants/nations';
+	import { copyToClipboard } from '$lib/utils';
+	import { queries } from '$lib/services/queries';
+	import type { NationDTO } from '$lib/services/apiClient';
+
+	const nationStates = queries.getNations();
+
+	const [_SOLANA, ...NATION_STATES] = _NATION_STATES;
 
 	let { citizenId, children } = $props();
 	const { firstName, lastName } = generateNamePair(citizenId);
 
-	// prepare umi to fetch assets
 	const umi = createUmi(PUBLIC_RPC_URL).use(mplCore());
 	const asset = fetchAssetV1(umi, citizenId);
 
-	let selectedCurrency: string = $state('');
-
-	async function copyToClipboard(text: string) {
-		try {
-			await navigator.clipboard.writeText(text);
-			toast.success(
-				`Copied to clipboard: ${text.substring(0, 10)}...${text.substring(text.length - 10, text.length)}`
-			);
-		} catch (err) {
-			console.error('Failed to copy: ' + text, err);
-		}
-	}
+	let selectedNation: string = $state('');
+	let stateCurrencyBalance = $state(0);
 
 	type CitizenAttribute =
 		| 'game'
@@ -50,6 +38,38 @@
 	function getAttribute(key: CitizenAttribute, asset: AssetV1) {
 		return asset.attributes?.attributeList.find((e) => e.key === key)?.value;
 	}
+	function getNationId(nationName: string) {
+		return NATION_STATES.findIndex((e) => e === nationName);
+	}
+	function getNationData() {
+		if (!selectedNation) return null;
+		const nation = $nationStates.data?.find((e) => {
+			return e.nationId === getNationId(selectedNation);
+		});
+		return nation;
+	}
+
+	function getRewardForStake(citizen: AssetV1) {
+		const { gdpRewardRate, stabilityRewardRate, healthcareRewardRate, environmentRewardRate } =
+			getNationData() as NationDTO;
+
+		const environment = getAttribute('environment_fix', citizen) as string;
+		const gdp = getAttribute('gdp_fix', citizen) as string;
+		const healthcare = getAttribute('healthcare_fix', citizen) as string;
+		const stability = getAttribute('stability_fix', citizen) as string;
+
+		const rewards = [
+			+environment * +environmentRewardRate,
+			+gdp * +gdpRewardRate,
+			+healthcare * +healthcareRewardRate,
+			+stability * +stabilityRewardRate
+		];
+
+		return {
+			rewards,
+			total: rewards.reduce((a, b) => a + b, 0)
+		};
+	}
 </script>
 
 <Dialog.Root>
@@ -61,7 +81,7 @@
 			<Dialog.Title>Stake your Citizen</Dialog.Title>
 			<Dialog.Description>
 				<div class="mt-2 flex flex-col gap-4">
-					{#await asset then data}
+					{#await asset then citizen}
 						<div class="flex items-center gap-2 rounded bg-panel p-2">
 							<img
 								src={`${CITIZEN_IMG_URL}${citizenId}`}
@@ -83,9 +103,9 @@
 						</div>
 						<p class="rounded bg-panel p-2">
 							Citizen of: <span class="group-hover:scale-125"
-								>{getCountryFlag(getAttribute('nation_state', data) ?? 'Solana')}</span
+								>{getCountryFlag(getAttribute('nation_state', citizen) ?? 'Solana')}</span
 							>
-							{getAttribute('nation_state', data)}
+							{getAttribute('nation_state', citizen)}
 						</p>
 
 						<div class="grid grid-cols-2 gap-2">
@@ -108,7 +128,7 @@
 												{' '}
 											{/if}
 										</span>
-										<p>{getAttribute(key, data)}</p>
+										<p>{getAttribute(key, citizen)}</p>
 									</div>
 								</div>
 							{/snippet}
@@ -123,21 +143,30 @@
 
 					<div class="grid grid-cols-2 items-start gap-4">
 						<div class="flex w-full flex-col gap-4">
-							<Combobox options={currencies} bind:value={selectedCurrency} />
+							<Combobox
+								options={NATION_STATES.map((e, i) => ({ value: e, label: e }))}
+								bind:value={selectedNation}
+							/>
 
-							{#if selectedCurrency !== ''}
-								<p>
-									{`$${selectedCurrency}`} balance:
-									{6}
-								</p>
+							{#if selectedNation !== ''}
+								<div class="flex justify-center">
+									<p>Current Balance: ${stateCurrencyBalance}</p>
+								</div>
 							{/if}
 						</div>
 
 						<div class="flex min-h-20 flex-col items-center justify-center rounded-xl bg-panel p-4">
-							{#if selectedCurrency !== ''}
-								<p class="text-xl font-bold">{10} {`$${selectedCurrency}`}</p>
-								<p class="font-thin">/per day</p>
-							{/if}
+							{#await asset then citizen}
+								{#if selectedNation !== ''}
+									{#key selectedNation}
+										<p class="text-xl font-bold">{getRewardForStake(citizen).total}</p>
+									{/key}
+									<p class="font-bold">
+										{`$${selectedNation.replace(/\b(?:and|of|the|Democratic)\b|\s+|'/gi, '')}`}
+									</p>
+									<p class="font-thin">/per day</p>
+								{/if}
+							{/await}
 						</div>
 					</div>
 
@@ -145,7 +174,7 @@
 						class="mt-4 w-full rounded-xl border-2 border-black bg-black p-2 transition-all hover:bg-black"
 					>
 						Stake for
-						{selectedCurrency === '' ? '...' : `$${selectedCurrency}`}
+						{selectedNation === '' ? '...' : `$${selectedNation}`}
 					</button>
 				</div>
 			</Dialog.Description>
