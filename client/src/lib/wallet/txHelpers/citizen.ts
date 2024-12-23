129d4d880035562ae659781749cbca6dd5462237
jollygrin <JollyGrin@protonmail.com>
2024-12-23 03:14:10 +0100

    add setuip for stake

--- client/src/lib/wallet/txHelpers/citizen.ts
+++ client/src/lib/wallet/txHelpers/citizen.ts
@@ -70,3 +70,70 @@ export async function mintNewCitizen(connection: Connection, address: string) {
 		message
 	};
 }
+
+export async function stakeCitizen(
+	connection: Connection,
+	address: string,
+	citizenAddress: string
+) {
+	const { SVPRGM } = initAnchor();
+
+	const pkey = new PublicKey(address);
+	const {
+		Uint8Array: gameIdInBytes,
+		gameAccountKey,
+		getGameMetaData
+	} = getGameAccount();
+
+	const gameMetaData = await getGameMetaData();
+	console.log({
+		gameMetaData,
+		bigNumber: gameMetaData.mintCost / LAMPORTS_PER_SOL
+	});
+
+	// const citizenAsset = anchor.web3.Keypair.generate();
+
+	const stakeCitizenIx = await SVPRGM.methods
+		.stakeOrUnstakeCitizen()
+		// .accountsPartial({
+		// 	playerAuthority: pkey,
+		// 	gameAccount: gameAccountKey, // get admin key
+		// 	worldAgentWallet: anchor.web3.PublicKey.findProgramAddressSync(
+		// 		[
+		// 			Buffer.from('wallet'),
+		// 			gameIdInBytes,
+		// 			gameMetaData.worldAgent.toBytes()
+		// 		],
+		// 		SVPRGM.programId
+		// 	)[0],
+		// 	collection: gameMetaData.collection,
+		// 	citizenAsset: citizenAsset.publicKey // check metaplex
+		// })
+		// .signers([citizenAsset])
+		.instruction();
+
+	const estimatedCU = await estimateCU(pkey, [stakeCitizenIx], connection);
+	const tx = new VersionedTransaction(
+		new TransactionMessage({
+			payerKey: pkey,
+			recentBlockhash: (await connection.getLatestBlockhash()).blockhash,
+			instructions: [
+				anchor.web3.ComputeBudgetProgram.setComputeUnitPrice({
+					microLamports: 1_000_000 // todo turn into constant
+				}),
+				anchor.web3.ComputeBudgetProgram.setComputeUnitLimit({
+					units: estimatedCU
+				}),
+				stakeCitizenIx
+			]
+		}).compileToLegacyMessage()
+	);
+
+	// tx.sign([citizenAsset]);
+	const message = Buffer.from(tx.message.serialize()).toString('base64');
+
+	return {
+		tx,
+		message
+	};
+}
